31c282f0cec4a748b54bd4bc8552c42f3f4e7369
firatbezir <bezir.1@osu.edu>
2023-11-24 14:55:44 +0300

    firat_dev branch has been created

---
+++ EasyCashIdentityProject.DataAccessLayer/Concrete/Context/Context.cs
@@ -0,0 +1,22 @@
+﻿using EasyCashIdentityProject.EntityLayer.Concrete;
+using Microsoft.AspNetCore.Identity.EntityFrameworkCore;
+using Microsoft.EntityFrameworkCore;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace EasyCashIdentityProject.DataAccessLayer.Concrete.Context
+{
+    public class Context : IdentityDbContext<AppUser, AppRole, int>
+    {
+        public Context(DbContextOptions<Context> options) : base(options)
+        {
+
+        }
+
+        public DbSet<CustomerAccount> CustomerAccounts { get; set; }
+        public DbSet<CustomerAccountProcess> CustomerAccountProcesses { get; set; }
+    }
+}
---
+++ EasyCashIdentityProject.EntityLayer/Concrete/CustomerAccountProcess.cs
@@ -0,0 +1,20 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace EasyCashIdentityProject.EntityLayer.Concrete
+{
+    // will vbe defined for customer account transactions
+    public class CustomerAccountProcess
+    {
+        public int CustomerAccountProcessID { get; set; }
+        public string TransactionType { get; set; }
+        public decimal Amount { get; set; }
+        public DateTime TransactionDate { get; set; }
+
+
+
+    }
+}
