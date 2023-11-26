c80dd9ae8c6b994c74e78882ae53448daa41ab16
firatbezir <bezir.1@osu.edu>
2023-11-26 19:30:29 +0300

    worked on Services

---
+++ EasyCashIdentityProject.BusinessLayer/Concrete/CustomerAccountProcessManager.cs
@@ -0,0 +1,46 @@
+﻿using EasyCashIdentityProject.BusinessLayer.Abstract;
+using EasyCashIdentityProject.DataAccessLayer.Abstract;
+using EasyCashIdentityProject.EntityLayer.Concrete;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace EasyCashIdentityProject.BusinessLayer.Concrete
+{
+    public class CustomerAccountProcessManager : ICustomerAccountProcessService
+    {
+        private readonly ICostumerAccountProcessDal _costumerAccountProcessDal;
+
+        public CustomerAccountProcessManager(ICostumerAccountProcessDal costumerAccountProcessDal)
+        {
+            _costumerAccountProcessDal = costumerAccountProcessDal;
+        }
+
+        public void TDelete(CustomerAccountProcess entity)
+        {
+            _costumerAccountProcessDal.Delete(entity);
+        }
+
+        public CustomerAccountProcess TGetByID(int id)
+        {
+            return _costumerAccountProcessDal.GetByID(id);
+        }
+
+        public List<CustomerAccountProcess> TGetList()
+        {
+            return _costumerAccountProcessDal.GetList();
+        }
+
+        public void TInsert(CustomerAccountProcess entity)
+        {
+            _costumerAccountProcessDal.Insert(entity);
+        }
+
+        public void TUpdate(CustomerAccountProcess entity)
+        {
+            _costumerAccountProcessDal.Update(entity);
+        }
+    }
+}
---
+++ EasyCashIdentityProject.DataAccessLayer/Repositories/GenericRepository.cs
@@ -0,0 +1,49 @@
+﻿using EasyCashIdentityProject.DataAccessLayer.Abstract;
+using EasyCashIdentityProject.DataAccessLayer.Concrete.Context;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace EasyCashIdentityProject.DataAccessLayer.Repositories
+{
+    public class GenericRepository<T> : IGenericDal<T> where T : class
+    {
+        private readonly Context _context;
+
+        public GenericRepository(Context context)
+        {
+            _context = context;
+        }
+        public void Delete(T entity)
+        {
+            _context.Set<T>().Remove(entity);
+            _context.SaveChanges();
+        }
+        public void Insert(T entity)
+        {
+            _context.Set<T>().Add(entity);
+            _context.SaveChanges();
+        }
+
+        public void Update(T entity)
+        {
+            _context.Set<T>().Update(entity);
+            _context.SaveChanges();
+        }
+
+
+        public T GetByID(int id)
+        {
+            return _context.Set<T>().Find(id);
+        }
+
+        public List<T> GetList()
+        {
+            return _context.Set<T>().ToList();
+        }
+
+
+    }
+}
