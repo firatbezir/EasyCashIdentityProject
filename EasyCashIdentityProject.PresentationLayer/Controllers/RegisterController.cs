cafb3939cc106b80ede9848f3200adbd546a5b1e
firatbezir <bezir.1@osu.edu>
2023-11-28 23:40:48 +0300

    register controller

---
+++ EasyCashIdentityProject.PresentationLayer/Controllers/RegisterController.cs
@@ -0,0 +1,21 @@
+﻿using EasyCashIdentityProject.EntityLayer.Concrete;
+using Microsoft.AspNetCore.Identity;
+using Microsoft.AspNetCore.Mvc;
+
+namespace EasyCashIdentityProject.PresentationLayer.Controllers
+{
+    public class RegisterController : Controller
+    {
+        private readonly UserManager<AppUser> _userManager;
+
+        public RegisterController(UserManager<AppUser> userManager)
+        {
+            _userManager = userManager;
+        }
+
+        public IActionResult Index()
+        {
+            return View();
+        }
+    }
+}
