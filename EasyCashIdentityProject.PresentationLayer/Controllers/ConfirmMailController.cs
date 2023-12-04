26ab0c5d8f0a98ea446ad3c2d05f142c62a1db65
firatbezir <bezir.1@osu.edu>
2023-12-04 23:45:20 +0300

    confirmation controller, con't

---
+++ EasyCashIdentityProject.PresentationLayer/Controllers/ConfirmMailController.cs
@@ -0,0 +1,20 @@
+﻿using Microsoft.AspNetCore.Mvc;
+
+namespace EasyCashIdentityProject.PresentationLayer.Controllers
+{
+	public class ConfirmMailController : Controller
+	{
+		[HttpGet]
+		public IActionResult Index()
+		{
+			return View();
+		}
+
+		[HttpPost]
+        public IActionResult Index()
+        {
+            return View();
+        }
+
+    }
+}
