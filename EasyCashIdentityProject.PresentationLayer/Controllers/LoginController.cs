908248441d2cf855120fccb39cc54c510483beb1
firatbezir <bezir.1@osu.edu>
2023-12-09 08:59:05 +0300

    user login done

---
+++ EasyCashIdentityProject.PresentationLayer/Controllers/LoginController.cs
@@ -0,0 +1,40 @@
+﻿using EasyCashIdentityProject.EntityLayer.Concrete;
+using EasyCashIdentityProject.PresentationLayer.Models;
+using Microsoft.AspNetCore.Identity;
+using Microsoft.AspNetCore.Mvc;
+
+namespace EasyCashIdentityProject.PresentationLayer.Controllers
+{
+    public class LoginController : Controller
+    {
+        private readonly SignInManager<AppUser> _signInManager;
+        private readonly UserManager<AppUser> _userManager;
+public LoginController(SignInManager<AppUser> signInManager, UserManager<AppUser> userManager)
+        {
+            _signInManager = signInManager;
+            _userManager = userManager;
+        }
+
+        [HttpGet]
+        public IActionResult Index()
+        {
+            return View();
+        }
+        [HttpPost]
+        public async Task<IActionResult> Index(LoginViewModel logInViewModel)
+        {
+            var result = await _signInManager.PasswordSignInAsync(logInViewModel.UserName, logInViewModel.Password, false, true);
+            if (result.Succeeded)
+            {
+                var user = await _userManager.FindByNameAsync(logInViewModel.UserName);
+                if (user.EmailConfirmed != false)
+                {
+                    return RedirectToAction("Index", "MyProfile");
+                }
+                //else: please confirm your email address
+            }
+            //else: either username or password is wrong
+            return View();
+        }
+    }
+}
