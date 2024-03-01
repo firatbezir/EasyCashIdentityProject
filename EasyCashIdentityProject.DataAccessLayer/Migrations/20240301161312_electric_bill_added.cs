a77d23b43d63f757d98bc815bfb1168495e3db9f
firatbezir <bezir.1@osu.edu>
2024-03-02 00:03:36 +0300

    bill payment ops will cont

---
+++ EasyCashIdentityProject.DataAccessLayer/Migrations/20240301161312_electric_bill_added.cs
@@ -0,0 +1,37 @@
+﻿using System;
+using Microsoft.EntityFrameworkCore.Migrations;
+
+#nullable disable
+
+namespace EasyCashIdentityProject.DataAccessLayer.Migrations
+{
+    public partial class electric_bill_added : Migration
+    {
+        protected override void Up(MigrationBuilder migrationBuilder)
+        {
+            migrationBuilder.CreateTable(
+                name: "ElectricBills",
+                columns: table => new
+                {
+                    ElectricBillId = table.Column<int>(type: "int", nullable: false)
+                        .Annotation("SqlServer:Identity", "1, 1"),
+                    ContractNumber = table.Column<string>(type: "nvarchar(max)", nullable: false),
+                    CustomerName = table.Column<string>(type: "nvarchar(max)", nullable: false),
+                    BillingPeriod = table.Column<string>(type: "nvarchar(max)", nullable: false),
+                    DueDate = table.Column<DateTime>(type: "datetime2", nullable: false),
+                    Amount = table.Column<decimal>(type: "decimal(18,2)", nullable: false),
+                    IsPaid = table.Column<bool>(type: "bit", nullable: false)
+                },
+                constraints: table =>
+                {
+                    table.PrimaryKey("PK_ElectricBills", x => x.ElectricBillId);
+                });
+        }
+
+        protected override void Down(MigrationBuilder migrationBuilder)
+        {
+            migrationBuilder.DropTable(
+                name: "ElectricBills");
+        }
+    }
+}
---
+++ EasyCashIdentityProject.EntityLayer/Concrete/ElectricBill.cs
@@ -0,0 +1,19 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace EasyCashIdentityProject.EntityLayer.Concrete
+{
+    public class ElectricBill
+    {
+        public int ElectricBillId { get; set; }
+        public string ContractNumber { get; set; }
+        public string CustomerName { get; set; }
+        public string BillingPeriod { get; set; }
+        public  DateTime DueDate { get; set; }
+        public decimal Amount { get; set; }
+        public bool IsPaid { get; set; }
+    }
+}
